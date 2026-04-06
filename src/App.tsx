5a2fc55e360ec935f4f934a01f545d0bfb68c43e
Yoftahe Abraham <jofab08@gmail.com>
2026-04-07 01:12:06 +0300

    chore: Add shadow borders

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export function App() {
   useRouteSync();
 
   return (
-    <div className="h-full flex flex-col bg-bg">
+    <div className="h-full flex flex-col bg-bg border border-[#444444]">
       <TitleBar />
       <Routes>
         <Route element={<AppLayout />}>
